788ef53ee50c28cee7079970b6a55a2c296e9edf
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-09-13 14:36:31 +0530

    feat: dummy-dashboard

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -75,7 +75,14 @@ export const userLogin = async(req: Request, res: Response):Promise<Response> =>
     }
 }
 
-
+export const dummyApi = (req: Request, res:Response) => {
+    try {
+        return res.status(200).json({ msg: "Hello from dummy API"});
+    } catch ( error ) {
+        console.log("Error in dummyApi", error);
+        return res.status(500).json({ msg: "Error in dummyApi",error });
+    }
+}
 
 
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, otpVerification, userLogin,  } from "@/controllers/userController";
+import { registerUser, otpVerification, userLogin, dummyApi } from "@/controllers/userController";
 import { verifyToken } from "@/middlewares/userAuthMiddleware";
 
 const userRoute = express.Router();
@@ -8,6 +8,6 @@ userRoute.post("/register/user", registerUser);
 userRoute.post("/register/verifyOtp/:id", otpVerification);
 
 userRoute.post("/user/login", userLogin);
-// userRoute.get("/dummy", verifyToken, dummyApi);
+userRoute.get("/dummy", verifyToken, dummyApi);
 
 export default userRoute;
