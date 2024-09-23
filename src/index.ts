408030be979c4f5316c3c59ec09b1f613410d827
kanan-logiciel <kanan@logiciel.io>
2024-09-23 11:28:54 +0530

    feat: company registration

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import userRoute from "./routes/userRoutes";
 import adminRoute from "./routes/adminRoutes";
+import companyRoute from "./routes/companyRoutes";
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +14,7 @@ Connection("mongodb://127.0.0.1:27017/InvoiceDB");
 app.use(express.json());
 app.use("/api", userRoute);
 app.use("/api", adminRoute);
+app.use("/api", companyRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
