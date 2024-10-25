7cf5565be64a120aae0e4e62f1d74f58d4a70b3c
kanan-logiciel <kanan@logiciel.io>
2024-10-25 14:11:12 +0530

    fix: modified code

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,3 @@
-import mongoose, { Document, Schema } from "mongoose";
 import mongoose, { Schema, Model, Document } from "mongoose";
 
 interface User extends Document {
@@ -9,12 +8,6 @@ interface User extends Document {
   password: string;
   plan: string;
   companyId: string;
-  companyId: string;
-  fullName: string;
-  email: string;
-  otp: number;
-  phoneNumber: string;
-  password: string;
 }
 
 const UserSchema: Schema<User> = new Schema({
@@ -23,17 +16,7 @@ const UserSchema: Schema<User> = new Schema({
   password: { type: String, minlength: 4, maxlength: 10 },
   email: { type: String, unique: true },
   otp: { type: Number },
-  phoneNumber: { type: String, minlength: 10, maxlength: 10 },
-});
-
-export const UserModel = mongoose.model<User>("User", UserSchema);
-
-const UserSchema: Schema<User> = new Schema({
-  companyId: { type: String },
-  fullName: { type: String, minlength: 5, maxlength: 15 },
-  password: { type: String, minlength: 4, maxlength: 10 },
-  email: { type: String, unique: true },
-  otp: { type: Number },
+  plan: { type: String },
   phoneNumber: { type: String, minlength: 10, maxlength: 10 },
 });
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,15 +1,4 @@
 import express from "express";
-import { validateRequest } from "@/middlewares/validationMiddleware";
-import { userValidation } from "@/schemas/userValidationSchema";
-import { createUserSchema } from "@/schemas/createUserSchema";
-import {
-  registerUserProfile,
-  registerUserEmail,
-  otpVerification,
-  userLogin,
-  dummyApi,
-  selectPlan,
-} from "@/controllers/userController";
 import { verifyToken } from "@/middlewares/userAuthMiddleware";
 import { validateRequest } from "@/middlewares/validationMiddleware";
 import { userValidation } from "@/schemas/userValidationSchema";
@@ -21,27 +10,8 @@ import {
   userLogin,
   dummyApi,
 } from "@/controllers/userController";
-import { verifyToken } from "@/middlewares/userAuthMiddleware";
 
 const userRoute = express.Router();
-console.log("ssd");
-userRoute.post(
-  "/register/userProfile/:id",
-  validateRequest(createUserSchema),
-  registerUserProfile
-);
-
-userRoute.post(
-  "/register/userEmail",
-  validateRequest(userValidation),
-  registerUserEmail
-);
-userRoute.post(
-  "/register/verifyOtp/:id",
-  validateRequest(userValidation),
-  otpVerification
-);
-console.log("ssd");
 userRoute.post(
   "/register/userProfile/:id",
   validateRequest(createUserSchema),
@@ -62,7 +32,7 @@ userRoute.post(
 userRoute.post("/user/login", validateRequest(userValidation), userLogin);
 userRoute.get("/dummy", validateRequest(userValidation), verifyToken, dummyApi);
 
-userRoute.post("/user/select-plan", selectPlan);
+// userRoute.post("/user/select-plan", selectPlan);
 userRoute.post("/user/login", validateRequest(userValidation), userLogin);
 userRoute.get("/dummy", validateRequest(userValidation), verifyToken, dummyApi);
 
