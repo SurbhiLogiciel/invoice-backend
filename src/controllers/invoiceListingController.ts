12f738f6aa30143c247e0e427575fa96b2210841
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-11-14 16:50:03 +0530

    feat: invoiceListing added

--- src/controllers/invoiceListingController.ts
+++ src/controllers/invoiceListingController.ts
@@ -7,10 +7,10 @@ export const invoiceListing = async (
   res: Response
 ): Promise<Response> => {
   try {
-    const { id } = req.params;
+    // const { id } = req.params;
     const { page = 1, limit = 10 } = req.query;
 
-    const userId = new mongoose.Types.ObjectId(id);
+    // const userId = new mongoose.Types.ObjectId(id);
 
     const pageNum = parseInt(page as string, 10);
     const limitNum = parseInt(limit as string, 10);
@@ -18,10 +18,10 @@ export const invoiceListing = async (
     const skip = (pageNum - 1) * limitNum;
 
     const list = await userModel
-      .find({ _id: userId })
+      .find()
       .skip(skip)
       .limit(limitNum);
-    const totalItems = await userModel.countDocuments({ _id: userId });
+    const totalItems = await userModel.countDocuments({ });
 
     return res.status(200).json({
       totalItems,
@@ -34,3 +34,10 @@ export const invoiceListing = async (
     return res.status(500).json({ msg: "Internal Server Error" });
   }
 };
+
+// export const updateStatus = async(req: Request, res: Response): Promise<Response> => {
+//   const { id } = req.params;
+//   const user = await userModel.findByIdAndUpdate({});
+
+// }
+
--- src/routes/invoiceRoutes.ts
+++ src/routes/invoiceRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { invoiceListing } from "@/controllers/invoiceListingController";
 const invoiceRouter = express.Router()
 
-invoiceRouter.get('/invoiceList/:id',invoiceListing);
+// invoiceRouter.get('/invoiceList/:id',invoiceListing);
+invoiceRouter.get("/invoiceList", invoiceListing);
 
 export default invoiceRouter;
